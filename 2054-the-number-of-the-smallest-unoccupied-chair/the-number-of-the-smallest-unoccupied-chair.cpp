9ea47f9134a95a99e286ad9c4c7a86f8ab1023da
Ankit Raj <126550981+99396ankitraj@users.noreply.github.com>
2024-10-12 15:11:31 +0530

    Time: 166 ms (35.49%) | Memory: 68.4 MB (30.37%) - LeetSync

--- 2054-the-number-of-the-smallest-unoccupied-chair/the-number-of-the-smallest-unoccupied-chair.cpp
+++ 2054-the-number-of-the-smallest-unoccupied-chair/the-number-of-the-smallest-unoccupied-chair.cpp
@@ -1,38 +1,33 @@
-struct Comparator{
-    bool operator()(const vector<int>&v1, const vector<int>&v2){
-        if(v1[1]==v2[1]){
-            return v1[0] > v2[0];
-        }
-        return v1[1]>v2[1];
-    }
-};
 class Solution {
 public:
-    int smallestChair(vector<vector<int>>& times, int idx) {
-        ios::sync_with_stdio(0); cin.tie(0);cout.tie(0);
-        priority_queue<vector<int>, vector<vector<int>>, Comparator>pq;
-        set<int> available;
-        int st = times[idx][0];
-        int n = times.size();
-        int cc = -1;
-        sort(times.begin(),times.end());
-        for(int i=0;i<n;i++){
-            while(pq.size() && pq.top()[1]<=times[i][0]){
-                available.insert(pq.top()[2]);
-                pq.pop();
-            }
-            if(available.size()){
-                int avb = *available.begin();
-                if(st==times[i][0])return avb;
-                pq.push({times[i][0],times[i][1],*available.begin()});
-                available.erase(avb);
-            }else{
-                cc++;
-                if(st==times[i][0])return cc;
-                pq.push({times[i][0],times[i][1],cc});
-            }
-        }
-        return -1;
-    }
+    int smallestChair(vector<vector<int>>& times, int targetFriend) {
+        int target = times[targetFriend][0];
+        sort(times.begin() ,times.end());
+        priority_queue<vector<int> , vector<vector<int>> , greater<vector<int>>> pq;
+        set<int> st;
+        int chair = 0;
 
+        for(auto itr : times){
+            int arival = itr[0];
+            int departure = itr[1];
+
+            while(!pq.empty() && arival >= pq.top()[0]){
+                 st.insert(pq.top()[1]);
+                 pq.pop();
+            }
+
+            if(arival == target) return !st.empty() ? *st.begin() : chair;
+            
+
+            if(!st.empty()){
+                pq.push({departure , *st.begin()});
+                st.erase(st.begin());
+            }
+            else{
+                pq.push({departure , chair++});
+            }
+
+        }
+        return 0;
+    }
 };
