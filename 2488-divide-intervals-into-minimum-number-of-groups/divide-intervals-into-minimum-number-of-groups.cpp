4defb5918d948b5ac2e9713c6b0ad6cfc86a123b
Ankit Raj <126550981+99396ankitraj@users.noreply.github.com>
2024-10-12 16:18:10 +0530

    Time: 470 ms (11.03%) | Memory: 126.8 MB (31.98%) - LeetSync

--- 2488-divide-intervals-into-minimum-number-of-groups/divide-intervals-into-minimum-number-of-groups.cpp
+++ 2488-divide-intervals-into-minimum-number-of-groups/divide-intervals-into-minimum-number-of-groups.cpp
@@ -3,7 +3,7 @@ public:
     int minGroups(vector<vector<int>>& times) {
         sort(times.begin(), times.end());
         priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>>pq;
-        set<int> st;
+        priority_queue<int , vector<int> , greater<int>> st;
         int chair = 0;
 
         for (auto itr : times) {
@@ -11,12 +11,12 @@ public:
             int departure = itr[1];
 
             while (!pq.empty() && arival > pq.top()[0]) {
-                st.insert(pq.top()[1]);
+                st.push(pq.top()[1]);
                 pq.pop();
             }
             if (!st.empty()) {
-                pq.push({departure, *st.begin()});
-                st.erase(st.begin());
+                pq.push({departure, st.top()});
+                st.pop();
             } else {
                 pq.push({departure, chair++});
             }
