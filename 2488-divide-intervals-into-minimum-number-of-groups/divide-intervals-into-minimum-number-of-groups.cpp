b91ca47b5fe6400b0a2aad7bdfb9bc47d07e5e66
Ankit Raj <126550981+99396ankitraj@users.noreply.github.com>
2024-10-12 17:35:58 +0530

    Time: 279 ms (64.86%) | Memory: 94.6 MB (75.23%) - LeetSync

--- 2488-divide-intervals-into-minimum-number-of-groups/divide-intervals-into-minimum-number-of-groups.cpp
+++ 2488-divide-intervals-into-minimum-number-of-groups/divide-intervals-into-minimum-number-of-groups.cpp
@@ -1,33 +1,23 @@
 class Solution {
 public:
-    int minGroups(vector<vector<int>>& times) {
+    int minGroups(vector<vector<int>>& times) { 
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+        int station = 0;
+        
         sort(times.begin(), times.end());
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>>pq;
-        priority_queue<int , vector<int> , greater<int>> st;
-        int chair = 0;
 
-        for (auto itr : times) {
-            int arival = itr[0];
+        for (const auto& itr : times) {
+            int arrival = itr[0];
             int departure = itr[1];
 
-            while (!pq.empty() && arival > pq.top().first) {
-                st.push(pq.top().second);
+            if (!pq.empty() && arrival > pq.top().first) {
+                pq.push({departure, pq.top().second});
                 pq.pop();
-            }
-            if (!st.empty()) {
-                pq.push({departure, st.top()});
-                st.pop();
             } else {
-                pq.push({departure, chair++});
+                pq.push({departure, station++});
             }
         }
-        return chair;
+        return station;
     }
 };
 
-auto init = []() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    return 'c';
-}();
