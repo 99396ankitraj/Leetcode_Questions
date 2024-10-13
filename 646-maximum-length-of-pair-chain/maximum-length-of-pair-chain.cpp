67ca3eaa3c70f0b56261c02c812715a3e9547780
Ankit Raj <126550981+99396ankitraj@users.noreply.github.com>
2024-10-13 13:19:46 +0530

    Time: 35 ms (85.45%) | Memory: 26.6 MB (67.60%) - LeetSync

--- 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.cpp
+++ 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.cpp
@@ -1,5 +1,10 @@
 class Solution {
 public:
+    Solution() {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+    }
     static bool cmp(const vector<int>& a, const vector<int>& b) {
         return a[1] < b[1];
     }
@@ -13,7 +18,6 @@ public:
                 currentEnd = pairs[i][1];
             }
         }
-
         return ans;
     }
 };
