813a21a1a3f78e24d5917c01f4d902442ddfba73
Ankit Raj <126550981+99396ankitraj@users.noreply.github.com>
2024-10-14 12:47:31 +0530

    Time: 80 ms (86.69%) | Memory: 61.3 MB (68.85%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -9,3 +9,9 @@ public:
         return pq.top();
     }
 };
+static const bool Booster = [](){
+    std::ios_base::sync_with_stdio(false);
+    std::cout.tie(nullptr);
+    std::cin.tie(nullptr);
+    return true;
+}();
