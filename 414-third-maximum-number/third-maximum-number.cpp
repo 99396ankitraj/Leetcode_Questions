b1aac04467b9788c83e546ac0f95eba7fc511d0e
Ankit Raj <126550981+99396ankitraj@users.noreply.github.com>
2024-10-14 23:25:50 +0530

    Time: 3 ms (85.93%) | Memory: 11.6 MB (97.72%) - LeetSync

--- 414-third-maximum-number/third-maximum-number.cpp
+++ 414-third-maximum-number/third-maximum-number.cpp
@@ -1,11 +1,44 @@
 class Solution {
 public:
     int thirdMax(vector<int>& nums) {
-        set<int , greater<int>> pq(nums.begin() ,nums.end());
-        if(pq.size() < 3) return *pq.begin();
-        pq.erase(pq.begin());
-        pq.erase(pq.begin());
+
+        int n = nums.size();
+
+        if(n==1){
+            return nums[0];
+        }
+
+        if(n==2){
+            return nums[0]> nums[1]? nums[0]: nums[1];
+        }
+
+        long first = LONG_MIN, second = LONG_MIN, third = LONG_MIN;
+
+        for(int num : nums){
+
+
+            if (num == first || num == second || num == third) {
+                continue; 
+            }
+
+            if(num > first ){
+                third = second;
+                second = first;
+                first = num;
+              
+            }
+            else if(num> second){
+
+                third = second;
+                second = num;
+
+            }
+            else if(num > third){
+                third = num;
+            }
+        }
+
+        return third==LONG_MIN ? first : third;
         
-        return *pq.begin();
     }
 };
