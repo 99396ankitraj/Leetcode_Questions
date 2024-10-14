7428c426e1dd54555e72e167d07e02ad347c15f3
Ankit Raj <126550981+99396ankitraj@users.noreply.github.com>
2024-10-14 20:10:22 +0530

    Time: 95 ms (57.16%) | Memory: 57.8 MB (99.54%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -1,60 +1,31 @@
 class Solution {
 public:
-    
-    int partition_algo(vector<int>& nums, int L, int R) {
-        
-        int P = nums[L];
-        int i = L+1; //0
-        int j = R; //0
-        
-        while(i <= j) {
-            
-            if(nums[i] < P && nums[j] > P) {
-                swap(nums[i], nums[j]);
-                i++;
-                j--;
-            }
-            
-            if(nums[i] >= P) {
-                i++;
-            }
-            
-            if(nums[j] <= P) {
-                j--;
-            }
-            
+    int pivotIndex(vector<int>&nums, int l, int r) {
+        int p = nums[l];
+        int i = l+1;
+        int j = r;
+
+        while(i <= j){
+            if(nums[i] < p && nums[j] > p)swap(nums[i++] , nums[j--]);
+            if(nums[i] >= p) i++;
+            if(nums[j] <= p) j--;
         }
-        
-        swap(nums[L], nums[j]);
-        return j; //P is at jth index
+        swap(nums[l] ,nums[j]);
+        return j;
     }
-    
     int findKthLargest(vector<int>& nums, int k) {
-        int n = nums.size();
-        
-        int L = 0;
-        int R = n-1;
-        
-        int pivot_idx = 0;
-        
-        //kth largest pivot element - nums[k-1] (descendinforder me partition karenge)
-        
-        while(true) {
-            
-             pivot_idx = partition_algo(nums, L, R);
-            
-            if(pivot_idx == k-1) {
+        int p = 0, l = 0, r = nums.size()-1;
+
+        while (true) {
+            p = pivotIndex(nums , l, r);
+
+            if (p == k - 1)
                 break;
-            } else if(pivot_idx > k-1) { //2nd larget , 4th larget
-                R = pivot_idx - 1;
-            } else {
-                L = pivot_idx + 1;
-            }
-            
+            else if (p > k - 1)
+                r = p-1;
+            else
+                l = p+1;
         }
-        
-        
-        return nums[pivot_idx];
-        
-    }
+        return nums[p];
+    }
 };
