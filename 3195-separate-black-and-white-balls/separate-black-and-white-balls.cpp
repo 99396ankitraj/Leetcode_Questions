8f57675d12f0a43f882cd99baffea519c8191298
Ankit Raj <126550981+99396ankitraj@users.noreply.github.com>
2024-10-15 13:29:20 +0530

    Time: 16 ms (98.53%) | Memory: 15.6 MB (45.68%) - LeetSync

--- 3195-separate-black-and-white-balls/separate-black-and-white-balls.cpp
+++ 3195-separate-black-and-white-balls/separate-black-and-white-balls.cpp
@@ -3,19 +3,11 @@ public:
     long long minimumSteps(string s) {
         ios::sync_with_stdio(0);
         cin.tie(0);
-        long long n = s.length() , ans = 0;
-        int i = 0 , j = s.length()-1;
-
-        while(i < j ){
-            if(s[i] != '0' && s[j] != '1'){
-                ans += (j-i);
-                swap(s[i++] , s[j--]);
-                
-            }
-            if(s[i] == '0') i++;
-            if(s[j] == '1') j--;
+        long long res=0,cur=0;
+        for(int i=0;i<s.length();++i){
+            if(s[i]=='0'){res+=i-cur;++cur;}
         }
-        
-        return ans;
+
+        return res;
     }
 };
