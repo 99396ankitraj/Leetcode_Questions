10fa7b7d2ae22dc93f68507dd58a506011085f72
Ankit Raj <126550981+99396ankitraj@users.noreply.github.com>
2024-10-29 11:21:13 +0530

    Time: 7 ms (35.43%) | Memory: 105.9 MB (23.37%) - LeetSync

--- 1116-maximum-level-sum-of-a-binary-tree/maximum-level-sum-of-a-binary-tree.cpp
+++ 1116-maximum-level-sum-of-a-binary-tree/maximum-level-sum-of-a-binary-tree.cpp
@@ -1,38 +1,35 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     int maxLevelSum(TreeNode* root) {
-        queue<TreeNode*>q;
-        int maxi = INT_MIN , level = 0 , ans = 0;
+        int maxSum = INT_MIN;
+        int ans = 0, level = 0;
+
+        queue<TreeNode*> q;
         q.push(root);
 
-        while(!q.empty()){
-            int sum = 0;
-            int len = q.size();
+        while (!q.empty()) {
             level++;
-
-            for(int i = 0 ; i < len ; i++){
+            int sumAtCurrentLevel = 0;
+            // Iterate over all the nodes in the current level.
+            for (int sz = q.size(); sz > 0; --sz) {
                 TreeNode* node = q.front();
                 q.pop();
-                sum += node->val;
-                if(node->left) q.push(node->left);
-                if(node->right) q.push(node->right);
+                sumAtCurrentLevel += node->val;
+
+                if (node->left != nullptr) {
+                    q.push(node->left);
+                }
+                if (node->right != nullptr) {
+                    q.push(node->right);
+                }
             }
-            if(maxi < sum){
-                maxi = sum;
+
+            if (maxSum < sumAtCurrentLevel) {
+                maxSum = sumAtCurrentLevel;
                 ans = level;
             }
         }
+
         return ans;
     }
 };
