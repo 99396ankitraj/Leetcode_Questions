8f335c3faa2f95a5eca5f78c1b0b41104e4af4f4
Ankit Raj <126550981+99396ankitraj@users.noreply.github.com>
2024-11-03 23:53:40 +0530

    Time: 7 ms (99.46%) | Memory: 33.6 MB (97.96%) - LeetSync

--- 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.cpp
+++ 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.cpp
@@ -1,26 +1,18 @@
 class Solution {
 public:
-    string makeFancyString(string s) {
-        int n = s.length(), i = 0, j = 1;
-        string ans = "";
-        if (n < 3) return s;
-        while (j < n) {
-            if (s[i] == s[j]) {
-                while (s[i] == s[j]) j++;
-                    ans += s[i];
-                    ans += s[i];
-                    i = j;
-                    j++;
-            } else {
-                ans += s[i];
-                i++;
-                j++;
+    string makeFancyString(string& s) {
+        if (s.size() < 3) return s;
+        int j = 2;
+        for (int i = 2; i < s.size(); ++i) {
+            if (s[i] != s[j - 1] || s[i] != s[j - 2]) {
+                s[j++] = s[i];
             }
         }
-        if(s[n-2] != s[n-1]) ans += s[n-1];
-        return ans;
+        s.resize(j);
+        return s;
     }
 };
+
 static const auto init = []() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
