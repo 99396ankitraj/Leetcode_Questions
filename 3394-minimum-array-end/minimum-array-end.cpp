e1ace508cf9ab37797e13be79af0d42476ce593b
Ankit Raj <126550981+99396ankitraj@users.noreply.github.com>
2024-11-17 14:18:48 +0530

    Time: 1732 ms (26.31%) | Memory: 8.5 MB (37.26%) - LeetSync

--- 3394-minimum-array-end/minimum-array-end.cpp
+++ 3394-minimum-array-end/minimum-array-end.cpp
@@ -1,17 +1,10 @@
 class Solution {
 public:
     long long minEnd(int n, int x) {
-        int count = 1 ;
-        long long num = x;
-        while(count != n){
-            long long  newNum = num + 1;
-            // cout<<newNum<<" ";
-            if(( num & newNum) != x){
-                newNum = x | newNum;
-            }
-            num = newNum;
-            count++;
-        }
-        return num;
+      long long num = x;
+      for(int i = 1 ; i < n ; i++){
+         num = (num+1) | x;
+      }
+      return num;  
     }
 };
