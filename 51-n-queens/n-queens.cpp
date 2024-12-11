9a79143337ff004607f1af2696050d3dfef7cfd1
Ankit Raj <126550981+99396ankitraj@users.noreply.github.com>
2024-12-11 14:12:07 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (59.60%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,42 +1,24 @@
 class Solution {
 public:
-    bool pathCheck(int row , int col , vector<string> &store , int n){
-        //check for upper diognal
-        int i = row;
-        int j = col;
-        while(i >= 0 && j >= 0){
-            if(store[i][j] == 'Q') return false;
-            i--;
-            j--;
-        }
-        //check for left
-        i = row;
-        j = col;
-        while(j >= 0){
-            if(store[i][j] == 'Q') return false;
-            j--;
-        }
-        //check for down diogonal
-        i = row;
-        j = col;
-        while(i < n && j >= 0){
-            if(store[i][j] == 'Q') return false;
-            i++;
-            j--;
-        }
-        return true;
-    }
-    void solve(int col , vector<string> &store , vector<vector<string>> &ans , int n){
+    void solve(int col , vector<string> &store , vector<vector<string>> &ans , int n , vector<int> &leftRow , vector<int> &upperDiogonal , vector<int> &lowerDiogonal){
         if(col == n){
             ans.push_back(store);
             return;
         }
 
         for(int row = 0 ; row < n ; row++){
-            if(pathCheck(row , col , store , n)){
+            if(leftRow[row] == 0 && lowerDiogonal[row+col] == 0 && upperDiogonal[n-1+col-row] == 0){
                 store[row][col] = 'Q';
-                solve(col+1 , store , ans , n);
+                leftRow[row] = 1;
+                lowerDiogonal[row+col] = 1;
+                upperDiogonal[n-1+col-row] = 1;
+
+                solve(col+1 , store , ans , n , leftRow ,upperDiogonal ,lowerDiogonal);
+
                 store[row][col] = '.';
+                leftRow[row] = 0;
+                lowerDiogonal[row+col] = 0;
+                upperDiogonal[n-1+col-row] = 0;
             }
         }
     }
@@ -44,8 +26,9 @@ public:
         vector<vector<string>> ans;
         string s(n , '.');
         vector<string> store(n , s);
+        vector<int> leftRow(n , 0) , upperDiogonal(2*n-1 ,0) , lowerDiogonal(2*n-1 , 0);
         
-        solve(0 , store , ans , n);
+        solve(0 , store , ans , n , leftRow , upperDiogonal , lowerDiogonal);
         return ans;
     }
 };
