fccc0bc4b43687475e3e4923399a90e6fccaacd1
Ankit Raj <126550981+99396ankitraj@users.noreply.github.com>
2024-12-11 12:12:30 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (30.29%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,28 +1,21 @@
 class Solution {
 public:
-    void solve(int ind , vector<vector<int>> &ans , vector<int>& nums , vector<int>&store , unordered_map<int ,int> &mp){
-        if(store.size() == nums.size()){
-            ans.push_back(store);
+    void solve(int ind , vector<vector<int>> &ans , vector<int>& nums ){
+        if(ind == nums.size()){
+            ans.push_back(nums);
             return;
         }
 
-        for(int i = 0 ; i < nums.size() ; i++){
-            if(!mp[nums[i]]){
-                mp[nums[i]] = true;
-                store.push_back(nums[i]);
-                solve(ind , ans , nums , store , mp);
-                store.pop_back();
-                mp[nums[i]] = false;
-            }
+        for(int i = ind ; i < nums.size() ; i++){
+           swap(nums[i],nums[ind]);
+           solve(ind+1 , ans , nums);
+           swap(nums[i],nums[ind]);
         }
         return;
     }
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
-        vector<int>store;
-        unordered_map<int ,int> mp;
-
-        solve(0 , ans , nums , store , mp);
+        solve(0 , ans , nums);
 
         return ans;
     }
