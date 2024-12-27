8b454e2f878d84a0131ff0832fcf4c2ad72b18fa
Ankit Raj <126550981+99396ankitraj@users.noreply.github.com>
2024-12-27 23:34:57 +0530

    Time: 0 ms (100.00%) | Memory: 20.9 MB (45.83%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -1,49 +1,34 @@
 class Solution {
 public:
-    int candy(vector<int>& ra) {
-         int n = ra.size();
-         int ans = 0;
-         vector<int>store(n , 1);
-         if(n == 1) return 1;
-         for(int i = 0 ; i < n ; i++){
-            if(i == 0){ 
-                if(ra[i] > ra[i+1]){
-                    ans += 2 ;
-                    store[i] = 2;
-                }else{
-                    ans+=1;
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+        if (n == 1) return 1;
+
+        int totalCandies = 1; // First child gets 1 candy
+        int up = 0, down = 0, peak = 0;
+
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                // Increasing slope
+                up++;
+                peak = up;
+                down = 0; // Reset down slope
+                totalCandies += up + 1;
+            } else if (ratings[i] < ratings[i - 1]) {
+                // Decreasing slope
+                down++;
+                up = 0; // Reset up slope
+                totalCandies += down;
+                if (down > peak) {
+                    totalCandies++;
                 }
+            } else {
+                // Flat slope
+                up = down = peak = 0;
+                totalCandies += 1;
             }
-            else if(i == n-1){
-                if(ra[i] > ra[i-1]){
-                    ans += (store[i-1]+1);
-                    store[i] = store[i-1] + 1;
-                }else{
-                    ans+=1;
-                }
-            }else{
-                if(ra[i] > ra[i-1]){
-                    ans += (store[i-1]+1);
-                    store[i] = store[i-1] + 1;
-                }else if(ra[i] > ra[i+1]){
-                     ans += 2;
-                     store[i] = 2;
-
-                     if(store[i] >= store[i-1] && ra[i] < ra[i-1]){
-                        int j = i;
-                        while(j > 0 && store[j] >= store[j-1] && ra[j] < ra[j-1]){
-                            ans-=store[j-1];
-                            store[j-1] = store[j]+1;
-                            ans += store[j-1];
-                            j--;
-                        }
-                     }
-
-                }else{
-                    ans += 1;
-                }
-            }   
         }
-        return ans;
+
+        return totalCandies;
     }
 };
