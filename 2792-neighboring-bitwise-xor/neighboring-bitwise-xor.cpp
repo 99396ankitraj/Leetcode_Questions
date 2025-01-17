0b630d562b60aa524f5eb295a861de2214afbd37
Ankit Raj <126550981+99396ankitraj@users.noreply.github.com>
2025-01-18 00:56:37 +0530

    Time: 31 ms (8.41%) | Memory: 275.6 MB (20.56%) - LeetSync

--- 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.cpp
+++ 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.cpp
@@ -4,11 +4,9 @@ public:
         int n = derived.size();
         vector<int> orignal(n);
         orignal[0] = 0;
-
         for(int i = 0 ; i < n-1 ; i++){
            derived[i] == 1 ? orignal[i+1] = ! orignal[i] : orignal[i+1] =  orignal[i]; 
         }
-
         for(int i = 0 ; i < n ; i++){
             if(derived[i] != orignal[i]^orignal[(i+1)%n]) return false;
         }
