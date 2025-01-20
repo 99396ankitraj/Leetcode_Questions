058562b6f3f0134f75015ff91cd0360206da8566
Ankit Raj <126550981+99396ankitraj@users.noreply.github.com>
2025-01-20 13:38:57 +0530

    Time: 1 ms (27.07%) | Memory: 26 MB (56.45%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,39 +1,19 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int n = height.size();
-        //firstly we will create max prefix array
-        vector<int>prefix(height);
-        prefix[0] = height[0];
-        for(int i = 1 ; i < n ; i++){
-            if(prefix[i-1] > height[i]){
-                prefix[i] = prefix[i-1];
+        int l = 0 , r = height.size()-1;
+        int leftmost = 0 , rightmost = 0 , total = 0;
+        while(l < r){
+            if(height[l] <= height[r]){
+                leftmost = max(leftmost , height[l]);
+                total += leftmost-height[l];
+                l++;
+            }else{
+                rightmost = max(rightmost , height[r]);
+                total += rightmost-height[r];
+                r--;
             }
         }
-
-        //now we will create suffix array
-        vector<int>suffix(height);
-        suffix[n-1] = height[n-1];
-        for(int i = n-2 ; i >= 0 ; i--){
-            if(suffix[i+1] > height[i]){
-                suffix[i] = suffix[i+1];
-            }
-        }
-
-        //now we will perform final step to get final answer
-        int ans = 0;
-        for(int i = 0 ; i < n ; i++){
-            ans += min(prefix[i] , suffix[i]) - height[i];
-        }
-
-        for(auto itr : prefix){
-            cout<<itr<<" ";
-        }
-        cout<<endl;
-        for(auto itr : suffix){
-            cout<<itr<<" ";
-        }
-        
-            return ans;
+        return total;
     }
 };
