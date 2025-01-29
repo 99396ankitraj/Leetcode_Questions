8c178550066a911273a42e4df64930522c2dc72e
Ankit Raj <126550981+99396ankitraj@users.noreply.github.com>
2025-01-29 22:40:08 +0530

    Time: 119 ms (78.75%) | Memory: 114.7 MB (52.08%) - LeetSync

--- 1612-avoid-flood-in-the-city/avoid-flood-in-the-city.cpp
+++ 1612-avoid-flood-in-the-city/avoid-flood-in-the-city.cpp
@@ -2,27 +2,30 @@ class Solution {
 public:
     vector<int> avoidFlood(vector<int>& rains) {
         int n = rains.size();
-        unordered_map<int, int> mp;
+        unordered_map<int,int> mp;
         set<int> st;
-        vector<int> ans(n, -1);
+        vector<int> ans(n , -1);
 
-        for (int i = 0; i < n; i++) {
-            if (rains[i] == 0) {
+        for(int i = 0 ; i < n ; i++){
+            if(rains[i] == 0){
                 st.insert(i);
-            } else {
-                if (mp.count(rains[i])) {
-                    auto it = st.upper_bound(mp[rains[i]]);
-                    if (it == st.end()) {
-                        return {};
-                    }
+                continue;
+            }
+            if(mp.count(rains[i])){
+                auto it = st.upper_bound( mp[rains[i]]);
+                if(it == st.end()){
+                    return {};
+                }else{
                     ans[*it] = rains[i];
                     mp[rains[i]] = i;
                     st.erase(*it);
                 }
+            }else{
                 mp[rains[i]] = i;
             }
         }
-        for (auto itr : st) {
+
+        for(auto itr : st){
             ans[itr] = 1;
         }
         return ans;
