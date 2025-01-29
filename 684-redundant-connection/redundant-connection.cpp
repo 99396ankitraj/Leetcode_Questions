30bca19a2f60ff0250568c14c50d06d9d9d6fa6b
Ankit Raj <126550981+99396ankitraj@users.noreply.github.com>
2025-01-30 01:18:10 +0530

    Time: 14 ms (10.98%) | Memory: 13.1 MB (55.83%) - LeetSync

--- 684-redundant-connection/redundant-connection.cpp
+++ 684-redundant-connection/redundant-connection.cpp
@@ -1,34 +1,32 @@
 class Solution {
 public:
-    int findParent(vector<int>& parent, int node) {
-        if (parent[node] == node) return node;
-        return parent[node] = findParent(parent, parent[node]); // Path compression
+    int findParent(vector<int>&parent , int node){
+        if(parent[node] == node) return node;
+        return  findParent(parent , parent[node]); 
     }
-    
-    void unionSet(vector<int>& parent, vector<int>& rank, int u, int v) {
-        if (rank[u] > rank[v]) parent[v] = u;
-        else if (rank[u] < rank[v]) parent[u] = v;
-        else {
+    void unionSet( vector<int>&parent ,  vector<int>&rank , int u , int v){
+        if(rank[u] > rank[v]) parent[v] = u;
+        else if(rank[u] < rank[v]) parent[u] = v;
+        else{
             rank[u]++;
             parent[v] = u;
         }
     }
-    
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
         int n = edges.size();
-        vector<int> parents(n + 1);
-        vector<int> rank(n + 1, 0);
+        vector<int>parents(n+1);
+        vector<int>rank(n+1,0);
 
-        for (int i = 0; i <= n; i++) parents[i] = i;
+        for(int i = 0 ; i <= n ; i++) parents[i] = i;
 
-        for (auto& edge : edges) {
-            int u = findParent(parents, edge[0]);
-            int v = findParent(parents, edge[1]);
-            if (u != v) {
-                unionSet(parents, rank, u, v);
-            } else {
-                return {edge[0], edge[1]};
+        for(auto itr : edges){
+            int u = findParent(parents , itr[0]);
+            int v = findParent(parents , itr[1]);
+            if(u != v){
+                unionSet(parents , rank , u , v);
+                cout<<"ankit"<<endl;
             }
+            else return {itr[0] ,itr[1]};
         }
 
         return {};
