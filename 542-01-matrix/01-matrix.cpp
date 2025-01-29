d7c4fbd27122f8846db065fc9662015335563303
Ankit Raj <126550981+99396ankitraj@users.noreply.github.com>
2025-01-30 02:53:31 +0530

    Time: 18 ms (63.30%) | Memory: 34.7 MB (75.31%) - LeetSync

---
+++ 542-01-matrix/01-matrix.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int n = mat.size();
+        int m = mat[0].size();
+
+        vector<vector<int>> ans(n , vector<int>(m , -1));
+        queue<pair<int , int>> q;
+
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < m ; j++){
+                if(mat[i][j] == 0){
+                    q.push({i,j});
+                    ans[i][j] = 0;
+                }
+            }
+        }
+
+        vector<pair<int,int>> dir = {{1,0} , {0,-1} , {0,1} , {-1,0}};
+
+        while(!q.empty()){
+            int i = q.front().first;
+            int j = q.front().second;
+            q.pop();
+
+            for(int ind = 0 ; ind < 4 ; ind++){
+                int nexti = i + dir[ind].first;
+                int nextj = j + dir[ind].second;
+
+                if(nexti >= 0 && nexti < n && nextj >= 0 && nextj < m && ans[nexti][nextj] == -1){
+                    q.push({nexti,nextj});
+                    ans[nexti][nextj] = ans[i][j] + 1;
+                }
+            }
+        }
+        return ans;
+    }
+};
