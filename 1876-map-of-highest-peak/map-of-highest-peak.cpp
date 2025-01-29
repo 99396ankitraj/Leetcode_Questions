5ea829a5a8a8cf7a558a59e962a250231d4f4339
Ankit Raj <126550981+99396ankitraj@users.noreply.github.com>
2025-01-30 02:36:48 +0530

    Time: 80 ms (37.42%) | Memory: 112.9 MB (55.60%) - LeetSync

--- 1876-map-of-highest-peak/map-of-highest-peak.cpp
+++ 1876-map-of-highest-peak/map-of-highest-peak.cpp
@@ -1,40 +1,37 @@
 class Solution {
 public:
     vector<vector<int>> highestPeak(vector<vector<int>>& isWater) {
-        int n = isWater.size(), m = isWater[0].size();
-        vector<vector<int>> ans(n, vector<int>(m, -1)); 
-        // maximize the maximum height
-        // 0 will remain 0
-        // we will try to take the diff to be 1 always untill it is not possible right?
-        // maybe multisource bfs
-        // put all the water cell in the queue
-        // the queue should contain the row and col
-        queue<pair<int, int>> q;
-        // insert all the zeroes in the queue
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
+        int n = isWater.size();
+        int m = isWater[0].size();
+        vector<vector<int>> ans(n , vector<int>(m , -1));
+        queue<pair<int,int>> q;
+
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < m ; j++){
                 if(isWater[i][j] == 1){
-                    q.push({i, j});
+                    q.push({i,j});
                     ans[i][j] = 0;
                 }
             }
         }
 
-        vector<pair<int, int>> dir = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-        // now start the multisource bfs
-        while(!q.empty()) {
-            auto [i, j] = q.front();
+        vector<pair<int,int>> dir = {{1,0} , {0,-1} , {0,1} , {-1,0}};
+
+        while(!q.empty()){
+            int i = q.front().first;
+            int j = q.front().second;
             q.pop();
-            
-            // push all the neighbours in the queue
-            for(const auto& [dx, dy]: dir) {
-                int ni = i + dx, nj = j + dy;
-                if(ni >= 0 && nj >= 0 && ni < n && nj < m && ans[ni][nj] == -1) {
-                    ans[ni][nj] = ans[i][j] + 1;
-                    q.push({ni, nj});
+
+            for(int ind = 0 ; ind < 4 ; ind++){
+                int nexti = i + dir[ind].first;
+                int nextj = j + dir[ind].second;
+                if(nexti >= 0 && nexti < n && nextj >= 0 && nextj < m && ans[nexti][nextj] == -1){
+                    q.push({nexti,nextj});
+                    ans[nexti][nextj] = ans[i][j]+1;
                 }
             }
         }
+
         return ans;
     }
 };
