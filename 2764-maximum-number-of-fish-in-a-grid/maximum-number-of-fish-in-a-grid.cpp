82d4d9c19bb26b94087db56d83125dd514afb861
Ankit Raj <126550981+99396ankitraj@users.noreply.github.com>
2025-01-29 12:35:57 +0530

    Time: 9 ms (55.44%) | Memory: 96.9 MB (41.62%) - LeetSync

--- 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.cpp
+++ 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.cpp
@@ -1,33 +1,28 @@
 class Solution {
 public:
-    void solve(int i , int j , vector<vector<int>>& grid , vector<pair<int,int>>dir , int n , int m , int &sum){
-            if(i < 0 || i >= n || j < 0 || j >= m || grid[i][j] == 0){
-                return ;
-            }
-            
-            sum += grid[i][j];   
-            grid[i][j] = 0;
+    int dfs(int i , int j , vector<vector<int>>& grid ,  vector<pair<int,int>>dir ,int m , int n){
+        int fish = grid[i][j];
+        grid[i][j] = 0;
 
-            for(int ind = 0 ; ind < 4 ; ind++){
-                int nexti = i + dir[ind].first;
-                int nextj = j + dir[ind].second;
-                solve(nexti , nextj , grid , dir , n , m , sum);
+        for(int ind = 0 ; ind < 4 ; ind++){
+            int nexti = i + dir[ind].first;
+            int nextj = j + dir[ind].second;
+            if(nexti >= 0 && nexti < n && nextj >= 0 && nextj < m && grid[nexti][nextj] != 0){
+                fish += dfs(nexti , nextj , grid , dir , m , n);
             }
-            return;
+        }
+        return fish;
     }
     int findMaxFish(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-
-        vector<pair<int,int>>dir = {{1,0} , {0,-1} , {0,1} , {-1,0}};
+        vector<pair<int,int>>dir = {{1,0} ,{0,-1} ,{0,1} ,{-1,0}};
         int maxi = 0;
 
         for(int i = 0 ; i < n ; i++){
             for(int j = 0 ; j < m ; j++){
                 if(grid[i][j] != 0){
-                    int sum = 0;
-                    solve(i , j , grid , dir , n , m , sum);
-                    maxi = max(maxi , sum);
+                    maxi = max(maxi ,dfs(i , j , grid , dir , m , n));
                 }
             }
         }
