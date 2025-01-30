945fec706512f2561a49e20703877756a6909d53
Ankit Raj <126550981+99396ankitraj@users.noreply.github.com>
2025-01-30 18:35:15 +0530

    Time: 1 ms (54.10%) | Memory: 26.8 MB (32.06%) - LeetSync

--- 1396-count-servers-that-communicate/count-servers-that-communicate.cpp
+++ 1396-count-servers-that-communicate/count-servers-that-communicate.cpp
@@ -1,30 +1,46 @@
 class Solution {
 public:
+    vector<pair<int,int>> dir = {{1,0} , {0,-1} , {0,1} , {-1,0}};
+    void dfs(int i ,int j , vector<vector<int>>& grid , int n , int m , int &count){
+        if(i < 0 || i >= n || j < 0 || j >= m || grid[i][j] == 0) return;
+
+        count++;
+        grid[i][j] = 0;
+
+        for(auto itr : dir){
+            int nexti = i + itr.first;
+            int nextj = j + itr.second;
+            dfs(nexti , nextj , grid , n , m , count);
+        }
+        //return ;
+    }
     int countServers(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
         vector<int> row(n , 0);
         vector<int> col(m , 0);
-        int total = 0;
 
         for(int i = 0 ; i < n ; i++){
             for(int j = 0 ; j < m ; j++){
                 if(grid[i][j] == 1){
-                    total++;
                     row[i]++;
                     col[j]++;
                 }
             }
         }
 
-        int count = 0;
+        int ans = 0;
         for(int i = 0 ; i < n ; i++){
             for(int j = 0 ; j < m ; j++){
                 if(grid[i][j]){
-                    if(row[i] == 1 && col[j]==1) count++;
+                    int count = 0;
+                    dfs(i , j , grid , n , m , count);
+                    if(count > 1 || row[i] > 1 || col[j] > 1){
+                        ans += count;
+                    }
                 }
             }
         }
-        return total-count;
+        return ans;
     }
 };
