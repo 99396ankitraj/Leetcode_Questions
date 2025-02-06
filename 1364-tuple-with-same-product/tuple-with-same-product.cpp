bec1610840f1339e7e2761e83defb7c1c36adeb4
Ankit Raj <126550981+99396ankitraj@users.noreply.github.com>
2025-02-06 19:41:26 +0530

    Time: 1126 ms (5.01%) | Memory: 200.5 MB (6.27%) - LeetSync

---
+++ 1364-tuple-with-same-product/tuple-with-same-product.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    void generateSubsequences(int ind, vector<int>& nums, unordered_map<int, vector<pair<int, int>>>& productMap) {
+        if (ind == nums.size()) return;
+
+        for (int i = ind + 1; i < nums.size(); ++i) {
+            int product = nums[ind] * nums[i];
+            productMap[product].push_back({nums[ind], nums[i]});
+        }
+
+        generateSubsequences(ind + 1, nums, productMap);
+    }
+
+    int tupleSameProduct(vector<int>& nums) {
+        unordered_map<int, vector<pair<int, int>>> productMap;
+        generateSubsequences(0, nums, productMap);
+
+        int result = 0;
+
+        // Count valid tuples based on productMap
+        for (const auto& [product, pairs] : productMap) {
+            if (pairs.size() > 1) {
+                int count = pairs.size();
+                result += (count * (count - 1) / 2) * 8; // Combination count * 8 permutations
+            }
+        }
+        return result;
+    }
+};
