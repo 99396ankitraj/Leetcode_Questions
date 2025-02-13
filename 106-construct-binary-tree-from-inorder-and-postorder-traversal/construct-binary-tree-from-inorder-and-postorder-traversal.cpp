e07d9431819e91f52713e62bf6d4363971e9d40f
Ankit Raj <126550981+99396ankitraj@users.noreply.github.com>
2025-02-13 20:13:37 +0530

    Time: 3 ms (71.62%) | Memory: 27 MB (96.80%) - LeetSync

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -1,32 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     TreeNode* solve(vector<int>& inorder, vector<int>& postorder , int &ind , int s , int e){
         if(s > e) return NULL;
-        
-        // Find the root value from postorder traversal
-        int rootVal = postorder[ind];
-        TreeNode* root = new TreeNode(rootVal);
-        
-        // Move index to the previous node in postorder traversal
-        ind--;
-        
-        // Find the root in the inorder traversal
         int i = s;
-        for(; i <= e ; i++){
-            if(inorder[i] == rootVal) break;
+
+        for(; i < e ; i++){
+            if(postorder[ind] == inorder[i]) break;
         }
 
-        // Recursively build the right subtree (since postorder is left-right-root)
-        root->right = solve(inorder , postorder , ind , i + 1 , e);
-        
-        // Recursively build the left subtree
-        root->left = solve(inorder , postorder , ind , s , i - 1);
-        
+        TreeNode* root = new TreeNode(postorder[ind]);
+        ind--;
+        root->right = solve(inorder , postorder , ind , i+1 , e);
+        root->left = solve(inorder , postorder , ind , s , i-1);
+
         return root;
     }
-    
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-        int ind = postorder.size() - 1;
-        return solve(inorder , postorder , ind , 0 , inorder.size() - 1);
+        int ind = inorder.size()-1;
+        return solve(inorder , postorder , ind , 0 , postorder.size()-1);
     }
 };
