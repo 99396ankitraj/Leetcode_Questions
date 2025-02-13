d3714e789214668c8108b086470f060e6c361aaf
Ankit Raj <126550981+99396ankitraj@users.noreply.github.com>
2025-02-13 22:42:08 +0530

    Time: 156 ms (74.61%) | Memory: 195 MB (50.64%) - LeetSync

--- 2461-amount-of-time-for-binary-tree-to-be-infected/amount-of-time-for-binary-tree-to-be-infected.cpp
+++ 2461-amount-of-time-for-binary-tree-to-be-infected/amount-of-time-for-binary-tree-to-be-infected.cpp
@@ -1,65 +1,73 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     int amountOfTime(TreeNode* root, int start) {
-        // Check if root is NULL
-        if (!root) return 0;
-        
-        // BFS to find the parent of each node and locate the starting node
-        unordered_map<TreeNode*, TreeNode*> parent;
+        unordered_map<TreeNode* , TreeNode*> parent;
         queue<TreeNode*> q;
         q.push(root);
 
         TreeNode* temp = NULL;
-        while (!q.empty()) {
+
+        while(!q.empty()){
             auto node = q.front();
             q.pop();
-            if (node->val == start) {
+            if(node->val == start){
                 temp = node;
             }
-            if (node->left) {
+            if(node->left){
                 q.push(node->left);
                 parent[node->left] = node;
             }
-            if (node->right) {
+            if(node->right){
                 q.push(node->right);
                 parent[node->right] = node;
             }
         }
 
-        // Perform BFS from the starting node to calculate the time taken to infect all nodes
+        //now we will perform bfs
         int count = 0;
-        unordered_map<TreeNode*, bool> vis;
+        unordered_map<TreeNode* , bool> vis;
         q.push(temp);
         vis[temp] = true;
 
-        while (!q.empty()) {
+        while(!q.empty()){
             int len = q.size();
             bool flag = false;
-            for (int i = 0; i < len; ++i) {
+            while(len--){
                 auto top = q.front();
                 q.pop();
-                // Check the left child
-                if (top->left && !vis[top->left]) {
+                //check for left part
+                if(top->left && !vis[top->left]){
                     q.push(top->left);
                     vis[top->left] = true;
                     flag = true;
                 }
 
-                // Check the right child
-                if (top->right && !vis[top->right]) {
+                //check for right part
+                if(top->right && !vis[top->right]){
                     q.push(top->right);
                     vis[top->right] = true;
                     flag = true;
                 }
 
-                // Check the parent
+                //check for parent part
                 if (parent.find(top) != parent.end() && !vis[parent[top]]) {
                     q.push(parent[top]);
                     vis[parent[top]] = true;
                     flag = true;
                 }
             }
-            if (flag) count++;
+                if(flag) count++;
         }
         return count;
     }
